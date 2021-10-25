24251353ec1dc5c99f76ddd7f30f42d0bb79aef3
jeremy.zhang <zhangyue9306@gmail.com>
2021-10-25 19:30:23 +0800

    update

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,14 +2,32 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	const prefix = "Hello, "
+	
+	prefix := greetingPrefix(language)
 	return prefix + name
 }
 
+func greetingPrefix(language string) string {
+	var prefix string
+	const spanishPrefix = "Hola, "
+	const englishPrefix = "Hello, "
+	const frenchPrefix = "Bonjour, "
+
+	switch language {
+		case "Spanish":
+			prefix = spanishPrefix
+		case "French":
+			prefix = frenchPrefix
+		default:
+			prefix = englishPrefix
+	}
+	return prefix
+}
+
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -11,16 +11,35 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Jacob")
+		got := Hello("Jacob", "")
 		want := "Hello, Jacob"
 
 		assertCode(t, got, want)
 	})
 
 	t.Run("hello with empty input", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCode(t, got, want)
 	})
+
+	t.Run("hello in Spanish", func(t *testing.T) {
+		got := Hello("Jacob", "Spanish")
+		want := "Hola, Jacob"
+
+		if got != want {
+			assertCode(t, got, want)
+		}
+	})
+
+	t.Run("hello in French", func(t *testing.T) {
+		got := Hello("Jacob", "French")
+		want := "Bonjour, Jacob"
+
+		if got != want {
+			assertCode(t, got, want)
+		}
+	})
+
 }
