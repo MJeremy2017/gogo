d213c2113ba9809492d8d31b450bfda622481aa1
jeremy.zhang <zhangyue9306@gmail.com>
2021-11-14 14:49:11 +0800

    add conc

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,13 +1,27 @@
 package concurrency
 
+
 // a functional type, all functions with same params and return match this type
 type WebsiteChecker func(string) bool
 
+type result struct {
+	url string
+	res bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChan := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChan <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChan
+		results[r.url] = r.res
 	}
 
 	return results
