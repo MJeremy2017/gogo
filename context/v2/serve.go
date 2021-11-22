18021c42478bf154dbe58feca984152d1f370c17
jeremy.zhang <zhangyue9306@gmail.com>
2021-11-22 11:05:13 +0800

    inject context in the function call

---
+++ context/v2/serve.go
@@ -0,0 +1,20 @@
+package mycontext
+
+import (
+	"net/http"
+	"context"
+	"fmt"
+)
+
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, _ := store.Fetch(r.Context())
+		fmt.Fprint(w, data)
+	}
+}
---
+++ context/v2/serve_test.go
@@ -0,0 +1,81 @@
+package mycontext
+
+import (
+	"net/http/httptest"
+	"net/http"
+	"testing"
+	"time"
+	"context"
+)
+
+type SpyStore struct {
+	response string
+	t testing.TB
+}
+
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		// simulate incrementally adding result
+		for _, c := range s.response {
+			select {
+			case <- ctx.Done():
+				s.t.Log("spy store got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
+
+}
+
+
+
+func TestServer(t *testing.T) {
+	t.Run("Server can run", func(t *testing.T) {
+		data := "Hello, World"
+		stub := &SpyStore{response: data, t: t}
+
+		svr := Server(stub)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf("got %q, want %q", response.Body.String(), data)
+		}
+
+	}) 
+
+	t.Run("Tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "Hello, World"
+		stub := SpyStore{response: data, t: t}
+
+		svr := Server(&stub)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancelFunc := context.WithCancel(request.Context())
+		time.AfterFunc(5 * time.Millisecond, cancelFunc)
+		// add cancelling context to request
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+		svr.ServeHTTP(response, request)
+
+		// stub.assertCancelled()
+
+	})
+
+}
