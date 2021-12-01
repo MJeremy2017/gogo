00933a9d6fffa51a47a77ece83d9f04ddfa6e22b
jeremy.zhang <zhangyue9306@gmail.com>
2021-12-01 10:40:45 +0800

    add minute

--- clockmath/clockface.go
+++ clockmath/clockface.go
@@ -20,12 +20,42 @@ func SecondHand(t time.Time) Point {
 	return p
 }
 
+func MinuteHand(t time.Time) Point {
+	p := minuteHandPoint(t)
+	p = Point{clockCenterX + minuteHandLength * p.X, clockCenterY - minuteHandLength * p.Y}
+	return p
+}
+
+func makeHand(p Point, length float64) Point {
+	p = Point{clockCenterX + length * p.X, clockCenterY - length * p.Y}
+	return p
+}
+
 func secondsInRadians(t time.Time) float64 {
 	return math.Pi / (30 / float64(t.Second()))
 }
 
+func minutesInRadians(t time.Time) float64 {
+	minute := float64(t.Minute())
+	second := float64(t.Second())
+	return minute * math.Pi / 30.0 + second * math.Pi / (60 * 30.0)
+}
+
+func hoursInRadians(t time.Time) float64 {
+	hour := float64(t.Hour() % 12) 
+	return hour * math.Pi/6 + minutesInRadians(t)/12
+}
 
 func secondHandPoint(t time.Time) Point {
 	radian := secondsInRadians(t)
-	return Point{math.Sin(radian), math.Cos(radian)}
+	return angleToPoint(radian)
+}
+
+func minuteHandPoint(t time.Time) Point {
+	radian := minutesInRadians(t)
+	return angleToPoint(radian)
+}
+
+func angleToPoint(angle float64) Point {
+	return Point{math.Sin(angle), math.Cos(angle)}
 }
