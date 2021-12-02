7e2618aa6feab75f480ae122a607021c80daa4dc
jeremy.zhang <zhangyue9306@gmail.com>
2021-12-02 10:58:15 +0800

    svg writer

--- clockmath/svgWriter.go
+++ clockmath/svgWriter.go
@@ -8,6 +8,8 @@ import (
 
 const (
 	secondHandLength = 90
+	minuteHandLength = 80
+	hourHandLength   = 50
 	clockCenterX     = 150
 	clockCenterY     = 150
 )
@@ -17,16 +19,26 @@ func SVGWriter(w io.Writer, t time.Time) {
 	io.WriteString(w, svgStart)
 	io.WriteString(w, bezel)
 	secondHand(w, t)
+	minuteHand(w, t)
+	hourHand(w, t)
 	io.WriteString(w, svgEnd)
 }
 
 func secondHand(w io.Writer, t time.Time) {
-	p := secondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
-	p = Point{p.X, -p.Y}
-	p = Point{p.X + clockCenterX, p.Y + clockCenterY} //translate
+	p := SecondHand(t)
 	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
+
+func minuteHand(w io.Writer, t time.Time) {
+	p := MinuteHand(t)
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+func hourHand(w io.Writer, t time.Time) {
+	p := HourHand(t)
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
 
 const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
