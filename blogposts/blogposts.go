0d7d7fc24c4cc151a197bb560d102ea6d344ec89
jeremy.zhang <zhangyue9306@gmail.com>
2021-12-06 11:04:55 +0800

    read descption

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -3,11 +3,19 @@ package blogposts
 import (
 	"io/fs"
 	"io"
+	"bufio"
+	"strings"
 )
 
 type Post struct {
 	Title string
+	Description string
 }
+
+const (
+	titleSeparator = "Title: "
+	descriptionSeparator = "Description: "
+)
 
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -17,8 +25,8 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 
 	posts := []Post{}
-	for _, d := range dir {
-		post, err := getPost(fileSystem, d)
+	for _, file := range dir {
+		post, err := getPost(fileSystem, file.Name())
 		if err != nil {
 			return nil, err
 		}
@@ -27,20 +35,28 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
 	// open file
-	postFile, err := fileSystem.Open(f.Name())
+	postFile, err := fileSystem.Open(fileName)
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
+	return newPost(postFile)
+}
 
+func newPost(postFile io.Reader) (Post, error) {
 	// read data
-	data, err := io.ReadAll(postFile)
-	if err != nil {
-		return Post{}, err
-	}
+	scanner := bufio.NewScanner(postFile)
 
-	post := Post{Title: string(data)[7:]}
+	readLine := func(prefix string) string {
+		scanner.Scan()
+		txt := scanner.Text()
+		return strings.TrimPrefix(txt, prefix)
+	}
+	title := readLine(titleSeparator)
+	description := readLine(descriptionSeparator)
+
+	post := Post{Title: title, Description: description}
 	return post, nil
 }
