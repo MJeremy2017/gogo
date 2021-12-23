db5cd503f5d688e9fca219f1a06c270ffa20c6f3
jeremy.zhang <zhangyue9306@gmail.com>
2021-12-23 19:52:25 +0800

    test

--- http-server/main.go
+++ http-server/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
 
 func main() {
-	store := NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem openning file %s %v", dbFileName, err)
+	}
+	store := NewFileSystemPlayerStore(db)
 	server := NewPlayerServer(store)
 	// handler := http.HandlerFunc(PlayerServer)  // cast into type HandlerFunc which has implemented serveHttp method already
 	log.Println("listen on port 5000")
