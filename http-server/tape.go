f674a14ede84bf3d037497134e9c54225f886821
jeremy.zhang <zhangyue9306@gmail.com>
2021-12-27 18:59:08 +0800

    tp

--- http-server/tape.go
+++ http-server/tape.go
@@ -10,7 +10,7 @@ type tape struct {
 }
 
 
-func (t *tape) Write(data []byte) (n int, err error) {
+func (t tape) Write(data []byte) (n int, err error) {
 	t.file.Truncate(0)
 	t.file.Seek(0, 0)
 	return t.file.Write(data)
