6a3f6681eff7e3fe4dd224500e5f837f50fcc48d
jeremy.zhang <zhangyue9306@gmail.com>
2022-01-05 20:49:35 +0800

    runanble main

--- http-server/cmd/cli/main.go
+++ http-server/cmd/cli/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+	"server/poker"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
-	fmt.Println("Hello")
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem openning file %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("got error %v", err)
+	}
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	poker.NewCLI(store, os.Stdin).PlayPoker()
 }
