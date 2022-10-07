786b81f5b5120c3d4fe16a72655bcc37d230c831
jeremy.zhang <zhangyue9306@gmail.com>
2022-10-07 13:53:23 +0800

    add timer play

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -11,25 +11,91 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 )
 
 const (
 	DataDir         = "data"
 	DefaultQuizFile = "problems.csv"
+	LimitSeconds    = 5
 )
+
+type GameTally struct {
+	correct int32
+	total   int32
+}
+
+func (g *GameTally) increaseCorrect() {
+	g.correct += 1
+}
+
+func (g *GameTally) increaseTotal() {
+	g.total += 1
+}
+
+func (g *GameTally) printScore() {
+	fmt.Printf("You have got %d out of %d quiz right\n", g.correct, g.total)
+}
 
 func main() {
 	fmt.Println("Quiz starting")
-	quizFileName := getQuizFileFromInput()
-	f := openQuizFile(quizFileName)
-	Play(f)
+	gameSeconds, quizFile := parseInArgs()
+	f := openQuizFile(quizFile)
+	PlayWithTimer(f, gameSeconds)
 }
 
-func getQuizFileFromInput() string {
+func parseInArgs() (int, string) {
+	var t int
 	var quizFileName string
+	flag.IntVar(&t, "seconds", LimitSeconds, "time limit")
 	flag.StringVar(&quizFileName, "quiz-file", DefaultQuizFile, "quiz file name")
 	flag.Parse()
-	return quizFileName
+	return t, quizFileName
+}
+
+func PlayWithTimer(f io.Reader, seconds int) {
+	done := make(chan int)
+	gameDuration := getGameDuration(seconds)
+	gameTally := NewGameTally()
+
+	// TODO add press enter to start the game
+	go PlayAsync(f, done, gameTally)
+	select {
+	case <-time.After(gameDuration):
+		fmt.Println("\nTime is up, you did not finish all the quiz!")
+	case <-done:
+		fmt.Println("\nYou have finished all the quiz!")
+		close(done)
+	}
+	gameTally.printScore()
+}
+
+func getGameDuration(seconds int) time.Duration {
+	return time.Duration(seconds) * time.Second
+}
+
+func NewGameTally() *GameTally {
+	return &GameTally{0, 0}
+}
+
+func PlayAsync(f io.Reader, done chan int, tally *GameTally) {
+	stdInReader := bufio.NewReader(os.Stdin)
+	csvReader := csv.NewReader(f)
+	for {
+		r, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		tally.increaseTotal()
+		quiz, expectedAnswer := r[0], r[1]
+		fmt.Printf("quiz: %s = ", quiz)
+
+		userAnswer := parseUserAnswer(stdInReader)
+		if answerIsCorrect(expectedAnswer, userAnswer) {
+			tally.increaseCorrect()
+		}
+	}
+	done <- 0
 }
 
 func Play(f io.Reader) {
