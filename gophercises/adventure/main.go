f66f3938644d284d4cf9c6d36e0c659055f60a37
jeremy.zhang <zhangyue9306@gmail.com>
2022-11-22 11:02:22 +0800

    org listening

--- gophercises/adventure/main.go
+++ gophercises/adventure/main.go
@@ -6,17 +6,23 @@ import (
 	"net/http"
 )
 
+const ADDRESS = ":8000"
+
+func main() {
+	mux := getRegisteredHandler()
+	log.Println("listening on port", ADDRESS)
+	log.Fatalln(http.ListenAndServe(ADDRESS, mux))
+}
+
+func getRegisteredHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", storyHandler)
+	return mux
+}
+
 func storyHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprint(w, "hello")
 	if err != nil {
 		log.Fatal(err)
-	}
-}
-
-func main() {
-	http.HandleFunc("/", storyHandler)
-	err := http.ListenAndServe("localhost:8000", nil)
-	if err != nil {
-		log.Fatalln(err)
 	}
 }
