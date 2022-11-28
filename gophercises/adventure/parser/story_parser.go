c5cdd1455fe7b155183af321ed2ffdd5cf610ceb
jeremy.zhang <zhangyue9306@gmail.com>
2022-11-28 10:53:37 +0800

    parse all strories in one struct

--- gophercises/adventure/parser/story_parser.go
+++ gophercises/adventure/parser/story_parser.go
@@ -5,60 +5,15 @@ import (
 	"os"
 )
 
-type Story struct {
-	Intro struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"intro"`
-	NewYork struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"new-york"`
-	Debate struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"debate"`
-	SeanKelly struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"sean-kelly"`
-	MarkBates struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"mark-bates"`
-	Denver struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"denver"`
-	Home struct {
-		Title   string        `json:"title"`
-		Story   []string      `json:"story"`
-		Options []interface{} `json:"options"`
-	} `json:"home"`
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
 }
 
 func ParseStory(fp string) (Story, error) {
