226d3babae40a45a696248f30af6b72234043c89
jeremy.zhang <zhangyue9306@gmail.com>
2022-11-29 11:07:51 +0800

    add handler for each chapter

--- gophercises/adventure/main.go
+++ gophercises/adventure/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"adventure/parser"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 // TODO: for each story, render a template (one method for all)
-// {story_name: {title, story, options: {text, arc_links}}}
 // host:port/story_name -> render template
 
 const ADDRESS = ":8000"
 const StoryFilePath = "story.json"
 const HtmlTemplatePath = "story_template.html"
 
+var story parser.Story
+var err error
+
 func main() {
+	story, err = parser.ParseStory(StoryFilePath)
+	LogFatalIfErr(err)
+
 	mux := getRegisteredHandler()
 	log.Println("listening on port", ADDRESS)
 	log.Fatal(http.ListenAndServe(ADDRESS, mux))
@@ -28,15 +34,29 @@ func getRegisteredHandler() http.Handler {
 }
 
 func storyHandler(w http.ResponseWriter, r *http.Request) {
-	story, err := parser.ParseStory(StoryFilePath)
-	LogFatalIfErr(err)
+	// TODO update link to direct to by chapter name
+	chapterKey := getRegisteredChapterKey(r)
+	chapter := story[chapterKey]
 
-	chapter := story["intro"]
 	tmpl, err := template.ParseFiles(HtmlTemplatePath)
 	LogFatalIfErr(err)
 
 	err = tmpl.Execute(w, chapter)
 	LogFatalIfErr(err)
+}
+
+func getRegisteredChapterKey(r *http.Request) string {
+	defaultKey := "intro"
+	key := r.URL.Path[1:]
+	fmt.Printf("op %s", key)
+	if len(key) == 0 || key == "home" {
+		return defaultKey
+	}
+	if _, ok := story[key]; !ok {
+		log.Printf("invalid chapter name %s", key)
+		return defaultKey
+	}
+	return key
 }
 
 func LogFatalIfErr(err error) {
