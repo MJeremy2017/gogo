bcd9ab11018a449c9c408a2b252700faf581359f
jeremy.zhang <zhangyue9306@gmail.com>
2022-12-10 16:20:31 +0800

    add more test cases

--- gophercises/link/parser_test.go
+++ gophercises/link/parser_test.go
@@ -30,28 +30,68 @@ func TestParser_ParseLinks(t *testing.T) {
 				}},
 			hasErr: false,
 		},
-		// TODO: fix the test
 		{
-			name: "can-get-link-from-multiple-html-data",
+			name: "can-get-link-from-a-single-html-data-2",
 			htmlData: `
-<a href="/dog">
-  <span>Something in a span</span>
-  Text not in a span
-  <b>Bold text!</b>
-</a>
-<a href="/cat">
-  <span>Something in a span</span>
-  <b>Bold text!</b>
-</a>
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
 `,
 			want: []Link{
 				{
-					Href: "/dog",
-					Text: "Something in a span Text not in a span Bold text!",
+					Href: "/other-page",
+					Text: "A link to another page",
+				}},
+			hasErr: false,
+		},
+		{
+			name: "can-get-link-from-multiple-html-data",
+			htmlData: `
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<h1>Social stuffs</h1>
+<div>
+    <a href="https://www.twitter.com/joncalhoun">
+        Check me out on twitter
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+    </a>
+    <a href="https://github.com/gophercises">
+        Gophercises is on <strong>Github</strong>!
+    </a>
+</div>
+</body>
+</html>
+`,
+			want: []Link{
+				{
+					Href: "https://www.twitter.com/joncalhoun",
+					Text: "Check me out on twitter",
 				},
 				{
-					Href: "/cat",
-					Text: "Something in a span Bold text!",
+					Href: "https://github.com/gophercises",
+					Text: "Gophercises is on Github !",
+				}},
+			hasErr: false,
+		},
+		{
+			name: "can-ignore-comment",
+			htmlData: `
+<html>
+<body>
+<a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+</body>
+</html>
+`,
+			want: []Link{
+				{
+					Href: "/dog-cat",
+					Text: "dog cat",
 				}},
 			hasErr: false,
 		},
