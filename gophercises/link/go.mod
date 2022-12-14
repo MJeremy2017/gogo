795ae923b8eb88f5d83226fd4dc32326f58799d2
jeremy.zhang <zhangyue9306@gmail.com>
2022-12-14 11:28:25 +0800

    add base queue

--- gophercises/link/go.mod
+++ gophercises/link/go.mod
@@ -3,9 +3,13 @@ module link
 go 1.18
 
 require (
+	github.com/Workiva/go-datastructures v1.0.53
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- gophercises/link/parser/sitemap.go
+++ gophercises/link/parser/sitemap.go
@@ -1,14 +1,61 @@
 package parser
 
 import (
-	"fmt"
+	"github.com/Workiva/go-datastructures/queue"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func BrowseLinks(url string) {
-	html, _ := GetHtmlPage(url)
-	fmt.Println(html)
+	visited := make(map[string]bool)
+	q := queue.New(1)
+	err := q.Put(url)
+	if err != nil {
+		log.Fatalf("failed to put url %s %v", url, err)
+	}
+	for q.Len() > 0 {
+		u, _ := q.Get(1)
+		strUrl := u[0].(string)
+		visited[strUrl] = true
+		//TODO saveUrl()
+
+		urls, err := ParseUrlLinks(strUrl)
+		if err != nil {
+			log.Printf("error parsring url %s %v", strUrl, err)
+			continue
+		}
+		for _, u := range urls {
+			if !visited[u] {
+				_ = q.Put(u)
+				visited[u] = true
+			}
+		}
+	}
+
+	// TODO parse links for each html
+	// q = [url]; q.pop(); getLinks(url); q.add(...)
+	//fmt.Println(html)
+}
+
+func ParseUrlLinks(url string) ([]string, error) {
+	data, err := GetHtmlPage(url)
+	if err != nil {
+		return nil, err
+	}
+	p := NewParser(strings.NewReader(data))
+	links, err := p.ParseLinks()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []string
+	for _, lk := range links {
+		urls = append(urls, lk.Href)
+	}
+	return urls, nil
 }
 
 func GetHtmlPage(url string) (string, error) {
