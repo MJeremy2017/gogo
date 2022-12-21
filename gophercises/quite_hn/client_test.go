07b373db8b67964be0a76130ff154e6f7f8a1ff8
jeremy.zhang <zhangyue9306@gmail.com>
2022-12-21 10:51:56 +0800

    base refactor

--- gophercises/quite_hn/client_test.go
+++ gophercises/quite_hn/client_test.go
@@ -10,10 +10,10 @@ import (
 func setup() (string, func()) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/topstories.json", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "[0,1,2,3,4]")
+		_, _ = fmt.Fprint(w, "[0,1,2,3,4]")
 	})
 	mux.HandleFunc("/item/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "{\"by\":\"test_user\",\"descendants\":10,\"id\":1,\"kids\":[16732999,16729637,16729517,16729595],\"score\":34,\"time\":1522599083,\"title\":\"Test Story Title\",\"type\":\"story\",\"url\":\"https://www.test-story.com\"}")
+		_, _ = fmt.Fprint(w, "{\"by\":\"test_user\",\"descendants\":10,\"id\":1,\"kids\":[16732999,16729637,16729517,16729595],\"score\":34,\"time\":1522599083,\"title\":\"Test Story Title\",\"type\":\"story\",\"url\":\"https://www.test-story.com\"}")
 	})
 	server := httptest.NewServer(mux)
 	return server.URL, func() {
