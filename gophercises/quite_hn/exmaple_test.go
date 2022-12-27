1a163b2a674f8ee10ff6f36d2d6777cb747fe70e
jeremy.zhang <zhangyue9306@gmail.com>
2022-12-27 16:38:15 +0800

    get async example to test

--- gophercises/quite_hn/exmaple_test.go
+++ gophercises/quite_hn/exmaple_test.go
@@ -6,21 +6,51 @@ package hn_test
 import (
 	"fmt"
 	"hn"
+	"math"
 	"testing"
+	"time"
 )
 
 // Test public facing interface
+func TestAsyncClient(t *testing.T) {
+	var client hn.Client
+	ids, err := client.TopItems()
+	if err != nil {
+		panic(err)
+	}
+
+	startTime := time.Now()
+	items, err := client.GetOrderedBatchItems(ids[:30])
+	if err != nil {
+		panic(err)
+	}
+
+	items = client.FilterStories(items)
+	fmt.Printf("Total stories %d \n", len(items))
+	for _, item := range items {
+		fmt.Printf("%s (by %s)\n", item.Title, item.By)
+	}
+	fmt.Printf("Total time %vs\n", NanoToSeconds(time.Since(startTime)))
+}
+
 func TestClient(t *testing.T) {
 	var client hn.Client
 	ids, err := client.TopItems()
 	if err != nil {
 		panic(err)
 	}
-	for i := 0; i < 5; i++ {
+
+	startTime := time.Now()
+	for i := 0; i < 30; i++ {
 		item, err := client.GetItem(ids[i])
 		if err != nil {
 			panic(err)
 		}
 		fmt.Printf("%s (by %s)\n", item.Title, item.By)
 	}
+	fmt.Printf("Total time %vs\n", NanoToSeconds(time.Since(startTime)))
+}
+
+func NanoToSeconds(t time.Duration) float64 {
+	return float64(t) / math.Pow(10, 9)
 }
