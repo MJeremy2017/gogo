223255cd202af0223966a0057f073b080f22bfd3
jeremy.zhang <zhangyue9306@gmail.com>
2023-01-05 11:00:09 +0800

    init

--- gophercises/tickets/viagogo/main.py
+++ gophercises/tickets/viagogo/main.py
@@ -1,6 +1,7 @@
 from gogokit import ViagogoClient
 
 # TODO get client ID and secret
+# waiting for the affiliate team to give client ID and secret (pending)
 # All methods require OAuth2 authentication. To get OAuth2 credentials for your
 # application, see http://developer.viagogo.net/#authentication.
 client = ViagogoClient("6779ef20e75817b79602", "")
