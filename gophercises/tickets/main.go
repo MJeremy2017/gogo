cd0c2c1507ebf7b46e911ca54ea06a313601147f
jeremy.zhang <zhangyue9306@gmail.com>
2023-02-07 10:50:54 +0800

    add server mux

--- gophercises/tickets/main.go
+++ gophercises/tickets/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"tickets/scrape"
 )
 
@@ -10,13 +11,39 @@ import (
 // TODO html page
 // event name | time | venue | cheapest ticket (quantity price) | platform
 
+type CombinedEvents struct {
+	ViagogoEvents *[]scrape.Event
+}
+
+func (c CombinedEvents) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprint(w, "Hello")
+}
+
+func NewCombinedEvents(viagogoEvents *[]scrape.Event) CombinedEvents {
+	return CombinedEvents{ViagogoEvents: viagogoEvents}
+}
+
 func main() {
+	events, err := scrapeViagogoTicket()
+	if err != nil {
+		log.Fatal(err)
+	}
+	combinedEvents := NewCombinedEvents(events)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", combinedEvents)
+
+	log.Fatal(http.ListenAndServe(":3000", mux))
+}
+
+func scrapeViagogoTicket() (*[]scrape.Event, error) {
 	baseUrl := "https://www.viagogo.com"
 	s := scrape.NewScraper(baseUrl)
 
 	events, err := s.GetEvents("/sg/Concert-Tickets/Rock-and-Pop/Grace-Jones-Tickets")
 	if err != nil {
 		log.Println(err)
+		return nil, err
 	}
 	for _, e := range events {
 		err := s.GetTickets(&e)
@@ -26,6 +53,7 @@ func main() {
 		}
 		display(&e)
 	}
+	return &events, nil
 }
 
 func display(e *scrape.Event) {
