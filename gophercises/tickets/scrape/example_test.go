e07b563c700455aeebabeb26947408de9961a62a
jeremy.zhang <zhangyue9306@gmail.com>
2023-03-03 09:58:38 +0800

    rm

--- gophercises/tickets/scrape/example_test.go
+++ gophercises/tickets/scrape/example_test.go
@@ -26,6 +26,40 @@ func TestStarHub(t *testing.T) {
 	fmt.Println(events[0])
 }
 
+func TestAsync(t *testing.T) {
+	// TODO
+	//var (
+	//	mu     sync.Mutex
+	//	number int
+	//)
+	//
+	//func main() {
+	//	// start the side calculation in a separate goroutine
+	//	go func() {
+	//		for {
+	//			// do the side calculation
+	//			time.Sleep(5 * time.Second)
+	//			mu.Lock()
+	//			number++
+	//			mu.Unlock()
+	//		}
+	//	}()
+	//
+	//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//		// acquire the lock to access the shared state
+	//		mu.Lock()
+	//		defer mu.Unlock()
+	//
+	//		// display the calculated number on the front page
+	//		tmpl := template.Must(template.ParseFiles("index.html"))
+	//		tmpl.Execute(w, number)
+	//	})
+	//
+	//	fmt.Println("Starting server on http://localhost:8080")
+	//	http.ListenAndServe(":8080", nil)
+	//}
+}
+
 func getAndSaveResponse(url string) {
 	response, err := http.Get(url)
 	defer response.Body.Close()
